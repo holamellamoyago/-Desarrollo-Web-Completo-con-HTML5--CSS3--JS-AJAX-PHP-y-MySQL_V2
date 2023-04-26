984947ad2e878db4c63217eb85f4ebc6114be7f5
holamellamoyago <yagootero2003@gmail.com>
2023-04-27 00:48:17 +0200

    27/04/23

--- FestivalMusica_inicio/gulpfile.js
+++ FestivalMusica_inicio/gulpfile.js
@@ -1,7 +1,12 @@
 
-const { src, dest, watch } = require('gulp')
+const { src, dest, watch, parallel } = require('gulp')
+
+//CSS
 const sass = require('gulp-sass')(require('sass'))
 const plumber = require('gulp-plumber')
+
+//IMAGENES
+const webp = require('gulp-webp')
 
 function css(done){
     src('src/scss/**/*.scss')    //Identificar el archivos de SASS
@@ -12,6 +17,19 @@ function css(done){
     done() //Callback quye avisa a gulp cuando llegamos al final. 
 }
 
+function versionWebp(done){
+
+    const opciones = {
+        quality: 50 
+    };
+
+    src('src/img/**/*.{png,jpg}')
+        .pipe (webp(opciones) )
+        .pipe( dest('build/img'))
+
+    done();
+}
+
 function dev(done){
     watch('src/scss/**/*.scss', css)
 
@@ -19,8 +37,9 @@ function dev(done){
 }
 
 
-exports.css = css
-exports.dev = dev
+exports.css = css;
+exports.versionWebp = versionWebp;
+exports.dev = parallel(versionWebp, dev ) ;
 
 
 
